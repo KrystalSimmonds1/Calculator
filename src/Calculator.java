08af70cb148c627c03b6aadfa6af78acc9a2f94e
Krystal <krystalsimmonds61@gmail.com>
2022-10-16 10:27:34 +0100

    Added if-else statements as a way to decide which operation should be done on the two numbers entered.
    Created multiplication, subtraction and division methods.

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        int firstNum, secondNum, sum;
+        int firstNum, secondNum, answer;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your first number: ");
@@ -11,12 +11,37 @@ public class Calculator {
         System.out.println("Enter your second number: ");
         secondNum = scanner.nextInt();
 
-        sum = addition(firstNum, secondNum);
+        System.out.println("Please select an operation: '1' for Addition, '2' for Multiplication, '3' for Subtraction and '4' for Division");
 
-        System.out.println(sum);
+        int choice;
+        choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println(addition(firstNum, secondNum));
+        } else if (choice == 2) {
+            System.out.println(multiplication(firstNum, secondNum));
+        } else if (choice == 3) {
+            System.out.println(subtraction(firstNum, secondNum));
+        } else if (choice == 4) {
+            System.out.println(division(firstNum, secondNum));
+        } else {
+            System.out.println("Invalid selection.");
+        }
     }
 
     public static int addition(int x, int y) {
         return x + y;
+    }
+
+    public static int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    public static int subtraction(int x, int y) {
+        return x - y;
+    }
+
+    public static double division(double x, double y) {
+        return x / y;
     }
 }
